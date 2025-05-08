f47edf22bea9d80bbe298ba850c405496db135aa
juampymdp <juampymdp@gmail.com>
2025-05-08 02:19:07 +0200

    TEST00

---
+++ Hello Vue/src/components/HeaderComp.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+    <header>
+        <img src="/images/Vue_logo.png" alt="Vue_logo" width=35px height="38";>
+    </header>
+</template>
+
+<style scoped>
+</style>
